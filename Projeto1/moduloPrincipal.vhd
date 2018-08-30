84f797f756ef6d8fef8d9151974e813da89679b6
Coutj <coutinholima.j@gmail.com>
2018-08-30 14:49:11 +0000

    Implementacao do somador de 4 bits (erros de variaveis persistem)

--- Projeto1/moduloPrincipal.vhd
+++ Projeto1/moduloPrincipal.vhd
@@ -30,23 +30,53 @@ use ieee.std_logic_1164.all;
 --use UNISIM.VComponents.all;
 
 entity Somador1Bit is
-	port (
-		numA			:	in 	std_logic;
-		numB			:	in 	std_logic;
-		carryIn		:	in 	std_logic;
-		carryOut		:	out 	std_logic;
-		resultado	:	out 	std_logic
-	);
-
+	port (	numA			:	in 	std_logic;
+				numB			:	in 	std_logic;
+				carryIn		:	in 	std_logic;
+				carryOut		:	out 	std_logic;
+				resultado	:	out 	std_logic);
 end Somador1Bit;
 
 architecture Somador1BitArch of Somador1Bit is
+
 begin
 
 	resultado <= numA xor numB xor carryIn;
-	carryOut <= (numA and numB) or ((numA xor numB) and carryIn); 
-
-
+	carryOut <= (numA and numB) or ((numA xor numB) and carryIn);
+	
 end Somador1BitArch;
 
+
+entity Somador4Bits is
+	
+	port (	numA4Bits		:in		std_logic_vector (3 downto 0);
+				numB4Bits		:in		std_logic_vector (3 downto 0);
+				cIn				:in		std_logic;
+				cOut				:out		std_logic;
+				resultado4Bits	:out		std_logic_vector (3 downto 0));
+				
+end Somador4Bits;
+
+architecture Somador4BitsArch of Somador4Bits is
+		
+	component Somador1Bit is
+	port (	numA4Bits		:	in 	std_logic;
+				numB4Bits		:	in 	std_logic;
+				--cIn				:	in 	std_logic;
+				cOut				:	out 	std_logic;
+				resultado4Bits	:	out 	std_logic);
+	end component;
+	
+	begin
+	
+		signal carryOut0, carryOut1, carryOut2, carryOut3	: std_logic;
+	
+		somador1:	Somador1Bit port map (numA4Bits(0), numB4Bits(0),'0', carryOut0, resultado4Bits(0));
+		somador2:	Somador1Bit port map (numA4Bits(1), numB4Bits(1), carryOut0, carryOut1, resultado4Bits(1));
+		somador3:	Somador1Bit port map (numA4Bits(2), numB4Bits(2), carryOut1, carryOut2, resultado4Bits(2));
+		somador4:	Somador1Bit port map (numA4Bits(3), numB4Bits(3), carryOut2, carryOut3, resultado4Bits(3));
+		
+		cOut <= carryOut3;
+
+end Somador4BitsArch;
 
